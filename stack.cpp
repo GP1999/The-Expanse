fb47a15f6d2d32907c3b4438b7c4ae2c36b6fcf5
GAURAV PONKIYA <39650633+GP1999@users.noreply.github.com>
2019-03-09 21:35:39 +0530

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,11 +1,13 @@
+//This program is of basic data structure stack.
 #include<iostream>
 #define MAX_SIZE 5
 using namespace std;
 int top=-1;
 int STACK[MAX_SIZE];
+//Insert element at the top of the stack
 void push(int x)
 {
-    if(top==MAX_SIZE-1)
+    if (top == MAX_SIZE-1)
         cout<<"Stack is full\n";
     else
     {
@@ -13,25 +15,28 @@ void push(int x)
 
     }
 }
+//Delete and return  element of top.
 int pop()
 {
-    if(top==-1)
+    if (top == -1)
         cout<<"Stack is empty\n";
     else
     {
         return STACK[top--];
     }
 }
+//return element of top
 int peep()
 {
-    if(top==-1)
+    if (top == -1)
         cout<<"Stack is empty\n";
     else
     return STACK[top];
 }
+//Display stack elements
 void display()
 {
-    if(top==-1)
+    if (top == -1)
        cout<<"Stack is empty\n";
     else
     {
@@ -48,7 +53,7 @@ int main()
     {
         cout<<"1.Push\n2.Pop\n3.Peep\n4.display\n5.Exit\nEnter choice:";
         cin>>choice;
-        switch(choice)
+        switch (choice)
         {
         case 1:
                cout<<"enter element:";
